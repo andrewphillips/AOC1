774938c747a9d426278f7ba5b3a3f2a0c96ba629
Andrew Phillips <andrewtylerphillips@gmail.com>
2012-10-11 21:19:56 -0400

    FIXED - changed names and it's fixed

--- Project-3/Project-3/ViewController.m
+++ Project-3/Project-3/ViewController.m
@@ -49,10 +49,10 @@
 // 5-Create a function called DisplayAlertWithString. This function will take as a parameter an NSString.
 - (void)DisplayAlertWithString:(NSString *)stringAlert
 {
-    UIAlertView *stringAlert = [[UIAlertView alloc] initWithTitle:@"Alert!" message:stringAlert delegate:nil cancelButtonTitle:@"Press Me!" otherButtonTitles:nil, nil];
-    if (stringAlert != nil)
+    UIAlertView *viewAlert = [[UIAlertView alloc] initWithTitle:@"Alert!" message:stringAlert delegate:nil cancelButtonTitle:@"Press Me!" otherButtonTitles:nil, nil];
+    if (viewAlert != nil)
     {
-        [stringAlert show];
+        [viewAlert show];
     }
 }
 
